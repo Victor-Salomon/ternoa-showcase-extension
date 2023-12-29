cb0ad0e33bf3e2adf0fa2721c02f80b5684b7c2c
Victor-Salomon <victor-salomon@hotmail.fr>
2023-12-29 15:29:14 +0100

    add link home in header

--- components/base/Header/Header.tsx
+++ components/base/Header/Header.tsx
@@ -1,17 +1,19 @@
 import TernoaIcon from "@/assets/providers/Ternoa";
 import Connection from "../Modals/Connection";
+import Link from "next/link";
 
 const Header = () => {
   return (
-      <nav className="container fixed top-0 left-0 right-0 backdrop-blur-md z-50">
-        <div className="flex justify-between items-center py-4">
-          <div className="flex items-center text-xl font-bold">
-            <TernoaIcon />
-            <div className="px-2">Ternoa</div>
-          </div>
-          <Connection />
-        </div>
-      </nav>
+    <nav className="container fixed top-0 left-0 right-0 backdrop-blur-md z-50">
+      <div className="flex justify-between items-center py-4">
+        <Link href={"/"} className="flex items-center text-xl font-bold">
+          <TernoaIcon />
+          <div className="px-2">Ternoa</div>
+        </Link>
+
+        <Connection />
+      </div>
+    </nav>
   );
 };
 
